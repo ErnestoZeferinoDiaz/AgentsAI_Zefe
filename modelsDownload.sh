08994438e035e41a6315e7b1cf89010b86654f82
ErnestoZeferinoDiaz <47879119+ErnestoZeferinoDiaz@users.noreply.github.com>
2026-06-20 18:47:08 -0600

    Create modelsDownload.sh

---
+++ modelsDownload.sh
@@ -0,0 +1,3 @@
+mkdir -p /workspace/models
+wget -P /workspace/models https://huggingface.co/unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF/resolve/main/Qwen3-Coder-30B-A3B-Instruct-Q5_K_M.gguf
+wget -P /workspace/models https://huggingface.co/unsloth/Qwen3.5-35B-A3B-GGUF/resolve/main/Qwen3.5-35B-A3B-Q5_K_M.gguf
